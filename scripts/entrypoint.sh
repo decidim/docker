2dd710ba3512932c3de343fc28b40f1fb0519402
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-02 19:04:51 -0300

    Enable shellcheck and fix issues

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,14 +5,17 @@ export BUNDLE_PATH=/gems
 export BUNDLE_BIN=/gems/bin
 export BUNDLE_APP_CONFIG=/gems/config
 
-export USER_UID=`stat -c %u /code/Gemfile`
-export USER_GID=`stat -c %g /code/Gemfile`
+USER_UID=$(stat -c %u /code/Gemfile)
+USER_GID=$(stat -c %g /code/Gemfile)
 
-usermod -u $USER_UID decidim 2> /dev/null
-groupmod -g $USER_GID decidim 2> /dev/null
-usermod -g $USER_GID decidim 2> /dev/null
+export USER_UID
+export USER_GID
 
-chown -R -h $USER_UID $BUNDLE_PATH 2> /dev/null
-chgrp -R -h $USER_GID $BUNDLE_PATH 2> /dev/null
+usermod -u "$USER_UID" decidim 2> /dev/null
+groupmod -g "$USER_GID" decidim 2> /dev/null
+usermod -g "$USER_GID" decidim 2> /dev/null
+
+chown -R -h "$USER_UID" "$BUNDLE_PATH" 2> /dev/null
+chgrp -R -h "$USER_GID" "$BUNDLE_PATH" 2> /dev/null
 
 /usr/bin/sudo -EH -u decidim "$@"
