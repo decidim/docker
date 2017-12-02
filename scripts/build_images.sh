d4a6affb475e37bab0f0599bba14cff621af9e62
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-02 19:04:51 -0300

    Enable shellcheck and fix issues

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -5,23 +5,23 @@ set -e
 sha1=${CIRCLE_SHA1:-latest}
 
 docker build -f Dockerfile \
-            --build-arg decidim_version=$DECIDIM_VERSION \
-            -t decidim:$sha1 \
+            --build-arg "decidim_version=$DECIDIM_VERSION" \
+            -t "decidim:$sha1" \
             --cache-from=decidim/decidim:latest .
 
 docker build -f Dockerfile-test \
-            --build-arg base_image=decidim:${CIRCLE_SHA1} \
-            --build-arg decidim_version=$DECIDIM_VERSION \
-            -t decidim:${CIRCLE_SHA1}-test \
+            --build-arg "base_image=decidim:$sha1" \
+            --build-arg "decidim_version=$DECIDIM_VERSION" \
+            -t "decidim:$sha1-test" \
             --cache-from=decidim/decidim:latest-test .
 
 docker build -f Dockerfile-dev \
-            --build-arg base_image=decidim:$sha1 \
-            --build-arg decidim_version=$DECIDIM_VERSION \
-            -t decidim:$sha1-dev \
+            --build-arg "base_image=decidim:$sha1" \
+            --build-arg "decidim_version=$DECIDIM_VERSION" \
+            -t "decidim:$sha1-dev" \
             --cache-from=decidim/decidim:latest-dev .
 
 docker build -f Dockerfile-deploy \
-            --build-arg base_image=decidim:$sha1 \
-            -t decidim:$sha1-deploy \
+            --build-arg "base_image=decidim:$sha1" \
+            -t "decidim:$sha1-deploy" \
             --cache-from=decidim/decidim:latest-deploy .
--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
-if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-  docker tag decidim:${CIRCLE_SHA1}-deploy decidim/decidim:latest-deploy
-  docker tag decidim:${CIRCLE_SHA1}-test decidim/decidim:latest-test
-  docker tag decidim:${CIRCLE_SHA1}-dev decidim/decidim:latest-dev
-  docker tag decidim:${CIRCLE_SHA1} decidim/decidim:latest
+if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
+  docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+
+  docker tag "decidim:${CIRCLE_SHA1}-deploy" decidim/decidim:latest-deploy
+  docker tag "decidim:${CIRCLE_SHA1}-test" decidim/decidim:latest-test
+  docker tag "decidim:${CIRCLE_SHA1}-dev" decidim/decidim:latest-dev
+  docker tag "decidim:${CIRCLE_SHA1}" decidim/decidim:latest
 
   docker push decidim/decidim:latest-deploy
   docker push decidim/decidim:latest-dev
@@ -16,15 +17,15 @@ if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
 fi
 
 if [[ "${CIRCLE_BRANCH}" =~ ^[0-9\.]+$ ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
+  docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-  docker tag decidim:${CIRCLE_SHA1}-deploy decidim/decidim:${CIRCLE_BRANCH}-deploy
-  docker tag decidim:${CIRCLE_SHA1}-test decidim/decidim:${CIRCLE_BRANCH}-test
-  docker tag decidim:${CIRCLE_SHA1}-dev decidim/decidim:${CIRCLE_BRANCH}-dev
-  docker tag decidim:${CIRCLE_SHA1} decidim/decidim:${CIRCLE_BRANCH}
+  docker tag "decidim:${CIRCLE_SHA1}-deploy" "decidim/decidim:${CIRCLE_BRANCH}-deploy"
+  docker tag "decidim:${CIRCLE_SHA1}-test" "decidim/decidim:${CIRCLE_BRANCH}-test"
+  docker tag "decidim:${CIRCLE_SHA1}-dev" "decidim/decidim:${CIRCLE_BRANCH}-dev"
+  docker tag "decidim:${CIRCLE_SHA1}" "decidim/decidim:${CIRCLE_BRANCH}"
 
-  docker push decidim/decidim:${CIRCLE_BRANCH}-deploy
-  docker push decidim/decidim:${CIRCLE_BRANCH}-dev
-  docker push decidim/decidim:${CIRCLE_BRANCH}-test
-  docker push decidim/decidim:${CIRCLE_BRANCH}
+  docker push "decidim/decidim:${CIRCLE_BRANCH}-deploy"
+  docker push "decidim/decidim:${CIRCLE_BRANCH}-dev"
+  docker push "decidim/decidim:${CIRCLE_BRANCH}-test"
+  docker push "decidim/decidim:${CIRCLE_BRANCH}"
 fi
