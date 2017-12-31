45cb8b1f041fbb894d33d0edcacf41d977ff1250
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-31 15:21:37 -0300

    Fix bad tags
    So we can generate images locally that can be directly used for local
    development, since `decidim/decidim` is what `docker-compose.yml` in
    decidim's repo wants.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -7,22 +7,22 @@ version=${DECIDIM_VERSION:-0.9.2}
 
 docker build -f Dockerfile \
             --build-arg "decidim_version=$version" \
-            -t "decidim:$sha1" \
+            -t "decidim/decidim:$sha1" \
             --cache-from=decidim/decidim:latest .
 
 docker build -f Dockerfile-test \
-            --build-arg "base_image=decidim:$sha1" \
+            --build-arg "base_image=decidim/decidim:$sha1" \
             --build-arg "decidim_version=$version" \
-            -t "decidim:$sha1-test" \
+            -t "decidim/decidim:$sha1-test" \
             --cache-from=decidim/decidim:latest-test .
 
 docker build -f Dockerfile-dev \
-            --build-arg "base_image=decidim:$sha1" \
+            --build-arg "base_image=decidim/decidim:$sha1" \
             --build-arg "decidim_version=$version" \
-            -t "decidim:$sha1-dev" \
+            -t "decidim/decidim:$sha1-dev" \
             --cache-from=decidim/decidim:latest-dev .
 
 docker build -f Dockerfile-deploy \
-            --build-arg "base_image=decidim:$sha1" \
-            -t "decidim:$sha1-deploy" \
+            --build-arg "base_image=decidim/decidim:$sha1" \
+            -t "decidim/decidim:$sha1-deploy" \
             --cache-from=decidim/decidim:latest-deploy .
