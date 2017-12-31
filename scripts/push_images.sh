c1cd0b4ddea530fbc9c3d07ff11ccd08dcf04f9f
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-31 15:21:37 -0300

    Fix bad tags
    So we can generate images locally that can be directly used for local
    development, since `decidim/decidim` is what `docker-compose.yml` in
    decidim's repo wants.

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -5,10 +5,10 @@ set -e
 if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
   docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-  docker tag "decidim:${CIRCLE_SHA1}-deploy" decidim/decidim:latest-deploy
-  docker tag "decidim:${CIRCLE_SHA1}-test" decidim/decidim:latest-test
-  docker tag "decidim:${CIRCLE_SHA1}-dev" decidim/decidim:latest-dev
-  docker tag "decidim:${CIRCLE_SHA1}" decidim/decidim:latest
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-deploy" decidim/decidim:latest-deploy
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-test" decidim/decidim:latest-test
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-dev" decidim/decidim:latest-dev
+  docker tag "decidim/decidim:${CIRCLE_SHA1}" decidim/decidim:latest
 
   docker push decidim/decidim:latest-deploy
   docker push decidim/decidim:latest-dev
@@ -19,10 +19,10 @@ fi
 if [[ "${CIRCLE_BRANCH}" =~ ^[0-9\.]+$ ]]; then
   docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-  docker tag "decidim:${CIRCLE_SHA1}-deploy" "decidim/decidim:${CIRCLE_BRANCH}-deploy"
-  docker tag "decidim:${CIRCLE_SHA1}-test" "decidim/decidim:${CIRCLE_BRANCH}-test"
-  docker tag "decidim:${CIRCLE_SHA1}-dev" "decidim/decidim:${CIRCLE_BRANCH}-dev"
-  docker tag "decidim:${CIRCLE_SHA1}" "decidim/decidim:${CIRCLE_BRANCH}"
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-deploy" "decidim/decidim:${CIRCLE_BRANCH}-deploy"
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-test" "decidim/decidim:${CIRCLE_BRANCH}-test"
+  docker tag "decidim/decidim:${CIRCLE_SHA1}-dev" "decidim/decidim:${CIRCLE_BRANCH}-dev"
+  docker tag "decidim/decidim:${CIRCLE_SHA1}" "decidim/decidim:${CIRCLE_BRANCH}"
 
   docker push "decidim/decidim:${CIRCLE_BRANCH}-deploy"
   docker push "decidim/decidim:${CIRCLE_BRANCH}-dev"
