18d30d0d575684a546fa1832ffe40d1d33fbcff9
David Rodríguez <deivid.rodriguez@riseup.net>
2018-05-20 17:16:42 -0300

    Allow passing extra build args to script
    For example, one might want to force base image to be pulled, and force
    rebuild of every layer without cache with
    
    ```console
    scripts/build_images.sh --pull --no-cache
    ```
    
    I used this to force building against the latest ruby official images.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -4,24 +4,29 @@ set -e
 
 sha1=${CIRCLE_SHA1:-latest}
 version=${DECIDIM_VERSION:-0.10.0}
+extra_args=("$@")
 
 docker build -f Dockerfile \
             --build-arg "decidim_version=$version" \
             -t "decidim/decidim:$sha1" \
-            --cache-from=decidim/decidim:latest .
+            --cache-from=decidim/decidim:latest \
+            "${extra_args[@]}" .
 
 docker build -f Dockerfile-test \
             --build-arg "base_image=decidim/decidim:$sha1" \
             --build-arg "decidim_version=$version" \
             -t "decidim/decidim:$sha1-test" \
-            --cache-from=decidim/decidim:latest-test .
+            --cache-from=decidim/decidim:latest-test \
+            "${extra_args[@]}" .
 
 docker build -f Dockerfile-dev \
             --build-arg "base_image=decidim/decidim:$sha1-test" \
             -t "decidim/decidim:$sha1-dev" \
-            --cache-from=decidim/decidim:latest-dev .
+            --cache-from=decidim/decidim:latest-dev \
+            "${extra_args[@]}" .
 
 docker build -f Dockerfile-deploy \
             --build-arg "base_image=decidim/decidim:$sha1" \
             -t "decidim/decidim:$sha1-deploy" \
-            --cache-from=decidim/decidim:latest-deploy .
+            --cache-from=decidim/decidim:latest-deploy \
+            "${extra_args[@]}" .
