7fb79dc75cc0a3f195f0130c79a021711c9b4154
David Rodríguez <deivid.rodriguez@riseup.net>
2018-06-20 13:11:41 -0300

    We don't want to use cache from remote images
    We should force using the images built in the same CI run.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -10,24 +10,20 @@ extra_args=("$@")
 docker build -f Dockerfile \
             --build-arg "decidim_version=$version" \
             -t "decidim/decidim:$sha1" \
-            --cache-from=decidim/decidim:latest \
             "${extra_args[@]}" .
 
 docker build -f Dockerfile-test \
             --build-arg "base_image=decidim/decidim:$sha1" \
             --build-arg "decidim_version=$version" \
             -t "decidim/decidim:$sha1-test" \
-            --cache-from=decidim/decidim:latest-test \
             "${extra_args[@]}" .
 
 docker build -f Dockerfile-dev \
             --build-arg "base_image=decidim/decidim:$sha1-test" \
             -t "decidim/decidim:$sha1-dev" \
-            --cache-from=decidim/decidim:latest-dev \
             "${extra_args[@]}" .
 
 docker build -f Dockerfile-deploy \
             --build-arg "base_image=decidim/decidim:$sha1" \
             -t "decidim/decidim:$sha1-deploy" \
-            --cache-from=decidim/decidim:latest-deploy \
             "${extra_args[@]}" .
