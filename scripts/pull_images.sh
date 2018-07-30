7b52c6e35d21a0d80d02048f5906a8d8bba73ac9
David Rodríguez <deivid.rodriguez@riseup.net>
2018-07-30 20:29:09 -0300

    Do we need pulling the images at all
    I think `docker build` does this automatically.

--- scripts/pull_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull decidim/decidim:latest || true
-docker pull decidim/decidim:latest-test || true
-docker pull decidim/decidim:latest-dev || true
-docker pull decidim/decidim:latest-deploy || true
