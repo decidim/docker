63cbd27e206d4183a7e8bcdaf92b282a7e1e1646
David Rodríguez <deivid.rodriguez@riseup.net>
2018-07-30 20:07:09 -0300

    Prefer long options
    In general, they make scripts more readable.

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -7,23 +7,23 @@ latest_version=$(curl https://rubygems.org/api/v1/versions/decidim/latest.json |
 version=${DECIDIM_VERSION:-$latest_version}
 extra_args=("$@")
 
-docker build -f Dockerfile \
+docker build --file Dockerfile \
             --build-arg "decidim_version=$version" \
-            -t "decidim/decidim:$sha1" \
+            --tag "decidim/decidim:$sha1" \
             "${extra_args[@]}" .
 
-docker build -f Dockerfile-test \
+docker build --file Dockerfile-test \
             --build-arg "base_image=decidim/decidim:$sha1" \
             --build-arg "decidim_version=$version" \
-            -t "decidim/decidim:$sha1-test" \
+            --tag "decidim/decidim:$sha1-test" \
             "${extra_args[@]}" .
 
-docker build -f Dockerfile-dev \
+docker build --file Dockerfile-dev \
             --build-arg "base_image=decidim/decidim:$sha1-test" \
-            -t "decidim/decidim:$sha1-dev" \
+            --tag "decidim/decidim:$sha1-dev" \
             "${extra_args[@]}" .
 
-docker build -f Dockerfile-deploy \
+docker build --file Dockerfile-deploy \
             --build-arg "base_image=decidim/decidim:$sha1" \
-            -t "decidim/decidim:$sha1-deploy" \
+            --tag "decidim/decidim:$sha1-deploy" \
             "${extra_args[@]}" .
