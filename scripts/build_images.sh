4afd6cd9119c31b2d534b9b929556b56b6c2e6de
David Rodríguez <deivid.rodriguez@riseup.net>
2018-07-30 20:08:32 -0300

    Align docker build parameters for prettyness

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -8,22 +8,22 @@ version=${DECIDIM_VERSION:-$latest_version}
 extra_args=("$@")
 
 docker build --file Dockerfile \
-            --build-arg "decidim_version=$version" \
-            --tag "decidim/decidim:$sha1" \
-            "${extra_args[@]}" .
+             --build-arg "decidim_version=$version" \
+             --tag "decidim/decidim:$sha1" \
+             "${extra_args[@]}" .
 
 docker build --file Dockerfile-test \
-            --build-arg "base_image=decidim/decidim:$sha1" \
-            --build-arg "decidim_version=$version" \
-            --tag "decidim/decidim:$sha1-test" \
-            "${extra_args[@]}" .
+             --build-arg "base_image=decidim/decidim:$sha1" \
+             --build-arg "decidim_version=$version" \
+             --tag "decidim/decidim:$sha1-test" \
+             "${extra_args[@]}" .
 
 docker build --file Dockerfile-dev \
-            --build-arg "base_image=decidim/decidim:$sha1-test" \
-            --tag "decidim/decidim:$sha1-dev" \
-            "${extra_args[@]}" .
+             --build-arg "base_image=decidim/decidim:$sha1-test" \
+             --tag "decidim/decidim:$sha1-dev" \
+             "${extra_args[@]}" .
 
 docker build --file Dockerfile-deploy \
-            --build-arg "base_image=decidim/decidim:$sha1" \
-            --tag "decidim/decidim:$sha1-deploy" \
-            "${extra_args[@]}" .
+             --build-arg "base_image=decidim/decidim:$sha1" \
+             --tag "decidim/decidim:$sha1-deploy" \
+             "${extra_args[@]}" .
