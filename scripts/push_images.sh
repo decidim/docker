aa5679e4a952a76da4b6b7a6b6f3fcf7eb9b5d20
David Rodríguez <deivid.rodriguez@riseup.net>
2018-08-08 08:19:04 -0300

    Require an explicit env var to run push script

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-latest_version=$(curl https://rubygems.org/api/v1/versions/decidim/latest.json | grep -o "[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*")
-version=${DECIDIM_VERSION:-$latest_version}
-
 if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
   docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-  docker push "decidim/decidim:$version-deploy"
-  docker push "decidim/decidim:$version-dev"
-  docker push "decidim/decidim:$version-test"
-  docker push "decidim/decidim:$version"
+  docker push "decidim/decidim:$DECIDIM_VERSION-deploy"
+  docker push "decidim/decidim:$DECIDIM_VERSION-dev"
+  docker push "decidim/decidim:$DECIDIM_VERSION-test"
+  docker push "decidim/decidim:$DECIDIM_VERSION"
 
   docker push decidim/decidim:latest-deploy
   docker push decidim/decidim:latest-dev
@@ -22,8 +19,8 @@ fi
 if [[ "${CIRCLE_BRANCH}" =~ ^[0-9\.]+$ ]]; then
   docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 
-  docker push "decidim/decidim:$version-deploy"
-  docker push "decidim/decidim:$version-dev"
-  docker push "decidim/decidim:$version-test"
-  docker push "decidim/decidim:$version"
+  docker push "decidim/decidim:$DECIDIM_VERSION-deploy"
+  docker push "decidim/decidim:$DECIDIM_VERSION-dev"
+  docker push "decidim/decidim:$DECIDIM_VERSION-test"
+  docker push "decidim/decidim:$DECIDIM_VERSION"
 fi
