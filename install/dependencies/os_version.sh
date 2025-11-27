fb9d9faa5db3e999314c0fd299f461791ff73120
David Igón <david@pokecode.net>
2025-11-27 12:17:45 +0100

    exit if not a correct distribution

--- install/dependencies/os_version.sh
+++ install/dependencies/os_version.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [ ! -f /etc/os-release ]; then
-  echo "Error: Unable to determine OS. /etc/os-release file not found."
-  echo "Installation stopped."
+echo "───────────────────────────────────────────────"
+echo "Now we are going to make sure that this distribution"
+echo "is based on Debian/Ubuntu."
+
+if [ $(uname) != "Linux" ]; then
+  echo "This installation process must be run on Linux"
   exit 1
 fi
 
-cat /etc/os-release
+if [ ! -n $(lsb_release -d | grep Ubuntu/Debian) ]; then
+  echo "This installation process must be run on a Debian/Ubuntu distribution."
+  exit 1
+fi
+
+echo "Correct distribution."
