a7d459ce8b0d1d248ce92325f7000fb6e3e9ae6b
David Igón <david@pokecode.net>
2025-12-09 15:48:28 +0100

    clean up a bit

--- install/up.sh
+++ install/up.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-composes="-f app.yml -f traefik.yml -f cache.yml -f worker.yml "
-
-if ! $EXTERNAL_DATABASE; then
-  composes+=" -f db.yml"
-fi
-
-if [ "$STORAGE" == 'local' ]; then
-  composes+=" -f storage.yml"
-fi
-
 echo "Starting containers..."
 docker compose --env-file .env up -d
 
