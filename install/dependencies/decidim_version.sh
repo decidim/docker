4e8a722c17af7c9276e98b3c4c9bc0fad92b11d0
David Igón <david@pokecode.net>
2025-12-22 12:53:37 +0100

    remove option to change decidim version, only install latest

--- install/dependencies/decidim_version.sh
+++ install/dependencies/decidim_version.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
 echo "───────────────────────────────────────────────"
-echo "📦 Choose Your Decidim Version"
+echo "📦 Decidim version"
 echo
 echo "💡 About Decidim versions:"
 echo "   • latest - Most recent stable release (recommended)"
-echo "   • 0.30, 0.28, etc. - Specific stable versions"
 echo "   • Custom images - Your own modified Decidim builds"
+echo
+echo "If you want, later on, you can modify the 'docker-compose.yml' to change the Decidim version"
 echo
 echo "Default image: decidim/decidim:latest"
 echo
-echo "Example options:"
-echo "  • decidim/decidim:latest (official stable)"
-echo "  • decidim/decidim:0.30 (specific version)"
-echo "  • ghcr.io/decidim/decidim:0.28 (GitHub Container Registry)"
-echo "  • ghcr.io/my-org/custom-decidim:1.0.0 (your custom build)"
-echo
-echo "🔍 Want to see available versions? Check: https://hub.docker.com/r/decidim/decidim/tags"
-echo
 
 while true; do
-  read -r -p "👉 Enter the Decidim image (or press Enter to use the default): " DECIDIM_IMAGE </dev/tty
+  read -r "👉 Click Enter to continue with the download of the Decidim image" </dev/tty
 
   DECIDIM_IMAGE=${DECIDIM_IMAGE:-decidim/decidim:latest}
 
